37239040a6649a712b40c4a0f224edceedc4f5fb
apathyaddict <121196231+apathyaddict@users.noreply.github.com>
2023-05-27 22:07:28 +0300

    deploy

--- index.js
+++ index.js
@@ -17,30 +17,23 @@ app.use('/users', usersRoute);
 
 // CORS middleware
 const allowCors = (fn) => async (req, res) => {
-    const allowedOrigins = ['http://localhost:8080', 'https://full-app-book-exchange-be.vercel.app'];
-    const origin = req.headers.origin;
-  
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      );
-  
-      if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-      }
-    } else {
-      // Handle invalid or disallowed origin
-      res.status(403).json({ error: 'Invalid origin' });
-      return;
-    }
-  
-    return await fn(req, res);
-  };
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  return await fn(req, res);
+};
 
 // Handler with CORS middleware
 const handler = allowCors((req, res) => {
