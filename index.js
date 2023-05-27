dca47774219df296b59a9fae2d9798dca3c74e72
apathyaddict <121196231+apathyaddict@users.noreply.github.com>
2023-05-27 22:09:04 +0300

    cors

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
 const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bookRoute = require('./routes/booksRoute');
-const usersRoute = require('./routes/usersRoute');
+require ('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
+
 const app = express();
+const cors =require('cors')
+const mongoose = require('mongoose')
+const bookRoute = require ('./routes/booksRoute')
+const usersRoute = require('./routes/usersRoute')
+const bodyParser = require("body-parser")
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors({ origin: ['http://localhost:8080','https://full-app-book-exchange-be.vercel.app']}))
+  
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw());
-app.use('/books', bookRoute);
-app.use('/users', usersRoute);
+app.use('/books', bookRoute)  
+app.use('/users', usersRoute)
 
-// CORS middleware
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // another common pattern
-   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  return await fn(req, res);
-};
-
-// Handler with CORS middleware
-const handler = allowCors((req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-});
-
-app.use(handler);
-
-// Connect to DB
+//connect to DB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log('Connected to DB and listening on port:', PORT);
-    });
-  })
-  .catch((error) => {
-    console.log('Error connecting to DB:', error);
-  });
+.then(()=>{
+    app.listen(PORT, () =>{
+        console.log('connected to DB and listening on port:', PORT)
+    })
+
+})
+.catch((error)=>{console.log(error)})
+
