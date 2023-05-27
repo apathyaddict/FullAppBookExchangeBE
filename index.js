cff2624b5b90df314f421369ccbd5a9e38c1e57f
apathyaddict <121196231+apathyaddict@users.noreply.github.com>
2023-05-27 22:01:48 +0300

    cors

--- index.js
+++ index.js
@@ -17,19 +17,22 @@ app.use('/users', usersRoute);
 
 // CORS middleware
 const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  const allowedOrigins = ['http://localhost:8080', 'https://full-app-book-exchange-be.vercel.app'];
+  const origin = req.headers.origin;
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
   }
 
   return await fn(req, res);
